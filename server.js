ebc8a1a6136b60192b7efcdb45838b3f4b1c4864
ckr1020 <rixin986@outlook.com>
2026-06-08 14:47:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 托管静态文件（你的 build 文件夹）
+// 解决 __dirname 在 ES 模块里不存在的问题
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// 托管静态文件
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // 根路径响应
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
